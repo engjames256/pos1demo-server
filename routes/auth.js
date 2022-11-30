f754990e265ec41470a507ea6bd7babc7572bdeb
Oyee James <engjames256@gmail.com>
2022-11-30 10:21:23 +0300

    Fixed the get Users route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, dashboard } from "../controllers/auth.js";
+import { register, login, dashboard, getUsers } from "../controllers/auth.js";
 import { authToken, authUser, authRole } from "../middleware/auth.js";
 import { ROLE } from "../permissions/role.js";
 
@@ -10,5 +10,6 @@ router
   .get(authToken, authUser, authRole(ROLE.MANAGER), dashboard);
 router.post("/register", register);
 router.post("/login", login);
+router.get("/users", getUsers );
 
 export default router;
